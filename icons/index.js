f2774ffc78189f778016ef2ed9f64339e0262f15
AdleeAfif <kamitokazuya@gmail.com>
2023-10-09 22:20:37 +0800

    Set icons for weather

---
+++ icons/index.js
@@ -0,0 +1,32 @@
+const weatherIcons = {
+  0: "fas fa-sun", // Clear sky
+  1: "fas fa-cloud-sun", // Mainly clear
+  2: "fas fa-cloud", // Partly cloudy
+  3: "fas fa-cloud", // Overcast
+  45: "fas fa-cloud-fog", // Fog
+  48: "fas fa-cloud-fog", // Depositing rime fog
+  51: "fas fa-cloud-showers-heavy", // Drizzle: Light
+  53: "fas fa-cloud-showers-heavy", // Drizzle: Moderate
+  55: "fas fa-cloud-showers-heavy", // Drizzle: Dense intensity
+  56: "fas fa-cloud-showers-heavy", // Freezing Drizzle: Light
+  57: "fas fa-cloud-showers-heavy", // Freezing Drizzle: Dense intensity
+  61: "fas fa-cloud-rain", // Rain: Slight
+  63: "fas fa-cloud-rain", // Rain: Moderate
+  65: "fas fa-cloud-showers-heavy", // Rain: Heavy intensity
+  66: "fas fa-snowflake", // Freezing Rain: Light
+  67: "fas fa-snowflake", // Freezing Rain: Heavy intensity
+  71: "fas fa-snowflake", // Snow fall: Slight
+  73: "fas fa-snowflake", // Snow fall: Moderate
+  75: "fas fa-snowflake", // Snow fall: Heavy intensity
+  77: "fas fa-snowflake", // Snow grains
+  80: "fas fa-cloud-showers-heavy", // Rain showers: Slight
+  81: "fas fa-cloud-showers-heavy", // Rain showers: Moderate
+  82: "fas fa-cloud-showers-heavy", // Rain showers: Violent
+  85: "fas fa-snowflake", // Snow showers: Slight
+  86: "fas fa-snowflake", // Snow showers: Heavy
+  95: "fas fa-bolt", // Thunderstorm: Slight or moderate
+  96: "fas fa-bolt", // Thunderstorm with slight hail
+  99: "fas fa-bolt", // Thunderstorm with heavy hail
+};
+
+module.exports = weatherIcons;
